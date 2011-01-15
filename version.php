4720b9119c0f9f326f33a9fe77c5c70fecba8546
Petr Skoda <commits@skodak.org>
2011-01-15 12:20:38 +0100

    improve release name

--- version.php
+++ version.php
@@ -8,6 +8,6 @@ $module->version  = 2008081403;  // The current module version (Date: YYYYMMDDXX
 $module->requires = 2007101512;  // Requires this Moodle version
 $module->cron     = 0;           // Period for cron to check this module (secs)
 
-$release = "1.9";             // User-friendly version number
+$release = "1.9 (20110115)";     // User-friendly version number
 
-?>
+
